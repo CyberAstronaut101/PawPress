14a7106ee7b7c290aed05c9f321bad004043e243
Elliot Mason <42648291+ejmason101@users.noreply.github.com>
2024-01-15 22:36:34 -0600

    added global error store; global error component

---
+++ frontend/Svelte/woofware-buttons/src/lib/components/GlobalError.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { errorStore } from '$lib/stores/errorStore.js';
+
+	$: if ($errorStore) {
+		setTimeout(() => {
+			errorStore.set(null);
+		}, 5000); // Clears the error after 5 seconds
+	}
+</script>
+
+{#if $errorStore}
+	<div class="error">
+		{$errorStore}
+	</div>
+{/if}
--- frontend/Svelte/woofware-buttons/src/routes/+layout.svelte
+++ frontend/Svelte/woofware-buttons/src/routes/+layout.svelte
@@ -2,10 +2,16 @@
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 
+	import ApiMessages from '$lib/components/ApiMessages.svelte';
+
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	import message from '$lib/ApiMessage';
+
+	import GlobalError from '$lib/components/GlobalError.svelte';
 </script>
 
 <!-- App Shell -->
@@ -28,11 +34,18 @@
 				</a>
 
 				<a class="btn btn-sm variant-ghost-surface" href="/history"> History </a>
+				<a class="btn btn-sm variant-ghost-surface" href="/audio-management"> Manage Audio </a>
 			</svelte:fragment>
 		</AppBar>
+
+		<GlobalError />
 	</svelte:fragment>
 	<!-- Page Route Content -->
 	<div id="main-wrapper" class="max-xl p-5">
 		<slot />
 	</div>
+
+	<!-- <svelte:fragment slot="pageFooter">
+		<ApiMessages />
+	</svelte:fragment> -->
 </AppShell>
