af6aa05cc31e1a79cc84c1cef8b432dfa8a8527f
Elliot Mason <42648291+ejmason101@users.noreply.github.com>
2024-01-21 15:24:19 -0600

    standalone svelte audio player components

---
+++ frontend/Svelte/woofware-buttons/src/lib/components/AudioClip.svelte
@@ -0,0 +1,45 @@
+<script>
+	/**
+	 * @type {{ name: string; description: string; file: string; } }}
+	 */
+	export let audioClip;
+
+	import AudioPlayer from '$lib/components/AudioPlayer.svelte';
+	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+</script>
+
+<div id="audioClipItem" class="flex flex-col justify-between variant-soft-surface p-3 my-2">
+	<h3 class="h3">{audioClip.name}</h3>
+	{audioClip.description ? audioClip.description : 'No Description for audio clip'}
+
+	<div class="flex align-items-center min-w-0 p-3">
+		<div class="min-w-0 flex-auto ml-3">
+			<AudioPlayer {...audioClip} />
+		</div>
+	</div>
+
+	<Accordion>
+		<AccordionItem>
+			<!-- <svelte:fragment slot="lead">(icon)</svelte:fragment> -->
+			<svelte:fragment slot="summary">Audio Clip Button Usage</svelte:fragment>
+			<svelte:fragment slot="content"
+				>TODO Get graph relationship to select all from the button objects with this audio mapped to
+				it</svelte:fragment
+			>
+		</AccordionItem>
+		<AccordionItem>
+			<!-- <svelte:fragment slot="lead">(icon)</svelte:fragment> -->
+			<svelte:fragment slot="summary">AudioClip Options</svelte:fragment>
+			<svelte:fragment slot="content">
+				<section>
+					<!-- <audio controls src={audioSrc} /> -->
+					<button type="button" class="btn variant-filled-error">Start Recording</button>
+					<button type="button" class="btn variant-filled-warning">Stop Recording</button>
+
+					<button type="button" class="btn variant-filled-primary">Submit</button>
+				</section>
+			</svelte:fragment>
+		</AccordionItem>
+		<!-- ... -->
+	</Accordion>
+</div>
